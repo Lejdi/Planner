61c8dd530de7e048f45d0f0c7fe3e0eb87e89609
fstarobrat <filip.starobrat@nbkcom.onmicrosoft.com>
2025-02-07 08:39:51 +0100

    Implement Add and Edit Task usecases

--- app/src/main/java/pl/lejdi/planner/business/usecases/edittask/EditTask.kt
+++ app/src/main/java/pl/lejdi/planner/business/usecases/edittask/EditTask.kt
@@ -1,4 +1,21 @@
 package pl.lejdi.planner.business.usecases.edittask
 
-class EditTask {
+import pl.lejdi.planner.business.data.cache.tasksdatasource.TasksDataSource
+import pl.lejdi.planner.business.data.cache.utl.CacheResult
+import pl.lejdi.planner.business.data.model.Task
+import pl.lejdi.planner.business.usecases.UseCase
+import pl.lejdi.planner.business.usecases.UseCaseResult
+import pl.lejdi.planner.framework.datasource.cache.model.task.TaskEntityMapper
+import pl.lejdi.planner.framework.presentation.util.ErrorType
+
+class EditTask(
+    private val tasksDataSource: TasksDataSource,
+    private val taskEntityMapper: TaskEntityMapper,
+) : UseCase<UseCaseResult<Unit>, Task>() {
+    override suspend fun execute(task: Task): UseCaseResult<Unit> {
+        val taskEntity = taskEntityMapper.mapFromBusinessModel(task)
+        val cacheResult = tasksDataSource.updateTask(taskEntity)
+        return if (cacheResult is CacheResult.Success) UseCaseResult.Success(Unit)
+        else UseCaseResult.Error(ErrorType.CacheError)
+    }
 }
