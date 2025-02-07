ea4a9bdb83f81a7373ca700d88405676c6262eb7
fstarobrat <filip.starobrat@nbkcom.onmicrosoft.com>
2025-02-07 08:37:52 +0100

    Implement Delete Task usecase

--- app/src/main/java/pl/lejdi/planner/business/usecases/edittask/DeleteTask.kt
+++ app/src/main/java/pl/lejdi/planner/business/usecases/edittask/DeleteTask.kt
@@ -1,4 +1,23 @@
 package pl.lejdi.planner.business.usecases.edittask
 
-class DeleteTask {
+import pl.lejdi.planner.business.data.cache.tasksdatasource.TasksDataSource
+import pl.lejdi.planner.business.data.cache.utl.CacheResult
+import pl.lejdi.planner.business.usecases.UseCase
+import pl.lejdi.planner.business.usecases.UseCaseResult
+import pl.lejdi.planner.framework.datasource.cache.model.task.TaskEntityMapper
+import pl.lejdi.planner.framework.presentation.common.model.task.TaskDisplayable
+import pl.lejdi.planner.framework.presentation.common.model.task.TaskDisplayableMapper
+import pl.lejdi.planner.framework.presentation.util.ErrorType
+
+class DeleteTask(
+    private val tasksDataSource: TasksDataSource,
+    private val taskEntityMapper: TaskEntityMapper,
+    private val taskDisplayableMapper: TaskDisplayableMapper,
+) : UseCase<UseCaseResult<Unit>, TaskDisplayable>() {
+    override suspend fun execute(taskToDelete: TaskDisplayable): UseCaseResult<Unit> {
+        val taskEntity = taskEntityMapper.mapFromBusinessModel(taskDisplayableMapper.mapToBusinessModel(taskToDelete))
+        val cacheResult = tasksDataSource.deleteTask(taskEntity)
+        return if (cacheResult is CacheResult.Success) UseCaseResult.Success(Unit)
+        else UseCaseResult.Error(ErrorType.CacheError)
+    }
 }
